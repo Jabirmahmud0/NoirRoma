0a7b4a7c4e69593d538880a0bae272061aab676c
unknown <mahmud15-5194@diu.edu.bd>
2026-04-24 22:16:47 +0600

    Added lenis

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,30 +2,33 @@ import "@/App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NoirRoma from "./pages/NoirRoma";
 import { Toaster } from "@/components/ui/sonner";
+import SmoothScroll from "./components/SmoothScroll";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<NoirRoma />} />
-        </Routes>
-      </BrowserRouter>
-      <Toaster
-        position="bottom-center"
-        toastOptions={{
-          style: {
-            background: "#121212",
-            color: "#F2F0E9",
-            border: "1px solid rgba(242, 240, 233, 0.1)",
-            borderRadius: "0px",
-            fontFamily: "Outfit, sans-serif",
-            fontWeight: 300,
-            letterSpacing: "0.05em",
-          },
-        }}
-      />
-    </div>
+    <SmoothScroll>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<NoirRoma />} />
+          </Routes>
+        </BrowserRouter>
+        <Toaster
+          position="bottom-center"
+          toastOptions={{
+            style: {
+              background: "#121212",
+              color: "#F2F0E9",
+              border: "1px solid rgba(242, 240, 233, 0.1)",
+              borderRadius: "0px",
+              fontFamily: "Outfit, sans-serif",
+              fontWeight: 300,
+              letterSpacing: "0.05em",
+            },
+          }}
+        />
+      </div>
+    </SmoothScroll>
   );
 }
 
